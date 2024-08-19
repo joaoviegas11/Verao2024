ae30cdfa454369eb3c86614eab31c66e5eef920b
joaoviegas11 <joaoviegas11@ua.pt>
2024-08-19 19:05:05 +0100

    aprender typescript

--- tipescripte/src/test.ts
+++ tipescripte/src/test.ts
@@ -1,2 +1,24 @@
-let age : number | boolean = 5;
+let age: number | boolean = 5;
 console.log(age);
+const sum = (x: number, y: number): string => (x + y).toString();
+type pessoa = {
+  ncc: number;
+  name?: string; //opcional
+};
+const user: pessoa = {
+  ncc: 1,
+  name: "Carlos",
+};
+const user2: pessoa = {
+  ncc: 1,
+};
+console.log(user.name);
+console.log(user2.name);
+console.log(user2.name!);
+interface CalcOp {
+  (x: number, y: number): number;
+}
+const add: CalcOp = (x: number, y: number) => x + y;
+const sub: CalcOp = (x: number, y: number) => x - y;
+const div: CalcOp = (x: number, y: number) => x / y;
+const mult: CalcOp = (x: number, y: number) => x * y;
